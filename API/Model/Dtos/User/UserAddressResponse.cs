feef6292a28b1ee728aa7a0369f875baca4cb008
nguyentankhoa <nguyentankhoa23082002@gmail.com>
2024-04-06 00:37:37 +0700

    add useraddress request,response

---
+++ API/Model/Dtos/User/UserAddressResponse.cs
@@ -0,0 +1,22 @@
+﻿using API.Services.Common;
+using System.ComponentModel;
+
+namespace API.Model.Dtos.User
+{
+    public class UserAddressResponse
+    {
+        public string Name { get; set; }
+        [DisplayName("Số điện thoại giao hàng")]
+        public string Contact { get; set; }
+        [DisplayName("Địa chỉ")]
+        public string Street { get; set; }
+        [DisplayName("Xã/Phường")]
+        public string Ward { get; set; }
+        [DisplayName("Quận/Huyện")]
+        public string District { get; set; }
+        [DisplayName("Tỉnh/Thành phố")]
+        public string Province { get; set; }
+    }
+
+    public class UserAddressObjectResponse : ObjectResponse<UserAddressResponse> { }
+}
